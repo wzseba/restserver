6f1718c82fa83d6527ef41fc5905a1ad4de73860
wzseba <wzseba@gmail.com>
2024-02-14 23:12:57 -0300

    refactor: se separo la logica de encryptacion

--- controllers/usuario.controller.js
+++ controllers/usuario.controller.js
@@ -1,14 +1,32 @@
 const { response } = require("express");
 
+const Usuario = require("../models/usuario");
+const { encryptPassword } = require("../helpers/hashPass");
+
 const getUsuarios = (req, res = response) => {
   res.json({ msg: "hola" });
 };
 
-const putUsuarios = (req, res = response) => {
-  res.json({ msg: "hola" });
+const postUsuarios = async (req, res = response) => {
+  try {
+    const { nombre, correo, password, rol } = req.body;
+    const user = new Usuario({ nombre, correo, password, rol });
+
+    //Encriptar password
+    user.password = encryptPassword(password);
+
+    //Guardar en DB
+    await user.save();
+
+    res.json({
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const postUsuarios = (req, res = response) => {
+const putUsuarios = (req, res = response) => {
   res.json({ msg: "hola" });
 };
 
