685e6a84f205f364f103ff59f6000cc95996382f
wzseba <wzseba@gmail.com>
2024-02-14 23:13:40 -0300

    add: configuarcion inicial DB

--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { dbConnection } = require("../database/config");
 
 class Server {
   constructor() {
@@ -7,11 +8,18 @@ class Server {
     this.port = process.env.PORT;
     this.usuariosPath = "/api/usuarios";
 
+    //conexion a DB
+    this.conectionDB();
+
     //middlewares
     this.middlewares();
 
     //rutas de app
     this.routes();
+  }
+
+  async conectionDB() {
+    await dbConnection();
   }
 
   middlewares() {
