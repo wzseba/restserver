47a15724d4aa09f9e6653745daac82c3fe10b9cb
wzseba <wzseba@gmail.com>
2024-02-14 23:10:24 -0300

    add: funcion para hash pass

---
+++ helpers/hashPass.js
@@ -0,0 +1,10 @@
+const bcryptjs = require("bcryptjs");
+
+const encryptPassword = (pass) => {
+  const salt = bcryptjs.genSaltSync();
+  return bcryptjs.hashSync(pass, salt);
+};
+
+module.exports = {
+  encryptPassword,
+};
