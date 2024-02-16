5ece512a8efc950c96f4f38466959acfb61fcc54
wzseba <wzseba@gmail.com>
2024-02-15 23:17:57 -0300

    add: se agrego funcion buscar usuario x id

--- validators/dbValidators.js
+++ validators/dbValidators.js
@@ -8,14 +8,22 @@ const esRoleValido = async (rol = "") => {
   }
 };
 
-const emailExiste = async (email) => {
-  const existeEmail = await Usuario.findOne({ email });
-  if (existeEmail) {
-    throw new Error("El email ya se encuentra registrado");
+const existeUsuarioPorId = async (id) => {
+  const existeUsuario = await Usuario.findById(id);
+  if (!existeUsuario) {
+    throw new Error("El usuario con ese id no existe");
   }
 };
 
+// const emailExiste = async (email) => {
+//   const existeEmail = await Usuario.findOne({ email });
+//   if (existeEmail) {
+//     throw new Error("El email ya se encuentra registrado");
+//   }
+// }; En el Schema de usuario tengo la propiedad unique: true lo que me impide crear duplicados
+
 module.exports = {
   esRoleValido,
-  emailExiste,
+  existeUsuarioPorId,
+  // emailExiste,
 };
