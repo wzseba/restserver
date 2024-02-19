8c1edbe10edc8b49ecc81352ce04b5c3b49fcc9c
wzseba <wzseba@gmail.com>
2024-02-18 22:47:06 -0300

    add: desarrollo de controller login

---
+++ controllers/auth.controller.js
@@ -0,0 +1,38 @@
+const { response } = require("express");
+const bcrypt = require("bcryptjs");
+const Usuario = require("../models/usuario");
+const { generarJWT } = require("../helpers/jsonWebToken");
+
+const loginUsuarios = async (req, res = response) => {
+  try {
+    const { correo, password } = req.body;
+    //Verifico si el email existe
+    const usuario = await Usuario.findOne({ correo });
+    if (!usuario) {
+      return res.status(400).json({ msg: "Correo incorrecto" });
+    }
+
+    //Si el usuario esta activo
+    if (!usuario.estado) {
+      return res.status(400).json({ msg: "El usuario no esta activo" });
+    }
+    //Verifico si la contraseña es correcta
+    const validPassword = bcrypt.compareSync(password, usuario.password);
+
+    if (!validPassword) {
+      return res.status(400).json({ msg: "Password incorrecto" });
+    }
+
+    //Genero el JWT
+    const token = generarJWT(usuario.id);
+
+    res.json({ usuario, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ error });
+  }
+};
+
+module.exports = {
+  loginUsuarios,
+};
