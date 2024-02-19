8922a395867f1c60267ec8cd9eb02df5ab0a0fc3
wzseba <wzseba@gmail.com>
2024-02-18 22:45:28 -0300

    add: desarrollo de validator para login

---
+++ validators/authValidators.js
@@ -0,0 +1,12 @@
+const { check } = require("express-validator");
+const { validationFieldResults } = require("./validatorField");
+
+const postAuthValidator = [
+  check("correo", "El correo es oligatorio").isEmail(),
+  check("password", "La contraseña es obligatoria").not().isEmpty(),
+  validationFieldResults,
+];
+
+module.exports = {
+  postAuthValidator,
+};
