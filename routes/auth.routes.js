0e47ca2f62861eee97a70a969d2af200de3aa753
wzseba <wzseba@gmail.com>
2024-02-21 23:02:54 -0300

    add: ruta google sign in

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,16 @@
 const { Router } = require('express');
-const { loginUsuarios } = require('../controllers/auth.controller');
-const { postAuthValidator } = require('../validators/authValidators');
+const {
+  loginUsuarios,
+  googleSignIn,
+} = require('../controllers/auth.controller');
+const {
+  postAuthValidator,
+  authGoogleSignIn,
+} = require('../validators/authValidators');
 
 const router = Router();
 
 router.post('/login', postAuthValidator, loginUsuarios);
+router.post('/google', authGoogleSignIn, googleSignIn);
 
 module.exports = router;
