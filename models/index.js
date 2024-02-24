81ae6a47e0e1a648b9a85d10d7b066acdb15d8db
wzseba <wzseba@gmail.com>
2024-02-24 11:25:25 -0300

    add: file barrel

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const Role = require('./role');
+const Categoria = require('./categoria');
+const Server = require('./server');
+const Usuario = require('./usuario');
+
+module.exports = {
+  Role,
+  Categoria,
+  Server,
+  Usuario,
+};
