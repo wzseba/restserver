37cc770f65b4845159f803aca3bfa1fe28af3ef1
wzseba <wzseba@gmail.com>
2024-02-24 08:50:27 -0300

    add: path categoria

--- models/server.js
+++ models/server.js
@@ -7,6 +7,7 @@ class Server {
     this.app = express();
     this.port = process.env.PORT;
     this.authPath = '/api/auth';
+    this.categoriaPath = '/api/categoria';
     this.usuariosPath = '/api/usuarios';
 
     // conexion a DB
@@ -36,6 +37,7 @@ class Server {
 
   routes() {
     this.app.use(this.authPath, require('../routes/auth.routes'));
+    this.app.use(this.categoriaPath, require('../routes/categoria.routes'));
     this.app.use(this.usuariosPath, require('../routes/usuario.routes'));
   }
 
