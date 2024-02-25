e236c462fbaba18da1344ec3ab12d86faa276ae8
wzseba <wzseba@gmail.com>
2024-02-25 14:42:41 -0300

    add: middlewares y controllers

--- routes/categoria.routes.js
+++ routes/categoria.routes.js
@@ -1,26 +1,30 @@
 const { Router } = require('express');
-const { createCategory } = require('../controllers/categoria.controller');
+const {
+  createCategory,
+  getCategories,
+  getCategory,
+  putCategory,
+  deleteCategory,
+} = require('../controllers/categoria.controller');
 const { validJTW } = require('../middlewares/jwtVerifications');
+const { validatorIdCategory } = require('../validators/categoriasValidator');
+const { adminRole } = require('../middlewares/isAdminRole');
 
 const router = Router();
 
 // Obtener todas las categorias - publico
-router.get('/', (req, res) => {
-  res.json({ msg: 'get' });
-});
+router.get('/', getCategories);
+
 // Obtener categoria por id - publico
-router.get('/:id', (req, res) => {
-  res.json({ msg: 'get' });
-});
+router.get('/:id', validatorIdCategory, getCategory);
+
 // Crear categoria - admin
-router.post('/', validJTW, createCategory);
+router.post('/', validJTW, adminRole, createCategory);
+
 // Actualizar categoria - admin
-router.put('/:id', (req, res) => {
-  res.json({ msg: 'put' });
-});
+router.put('/:id', validJTW, adminRole, validatorIdCategory, putCategory);
+
 // Borrar categoria - admin
-router.delete('/:id', (req, res) => {
-  res.json({ msg: 'delete' });
-});
+router.delete('/:id', validJTW, adminRole, validatorIdCategory, deleteCategory);
 
 module.exports = router;
