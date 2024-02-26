b7854ee1587e4c41a42af18d2a21a1326f65cdb4
wzseba <wzseba@gmail.com>
2024-02-26 19:14:28 -0300

    add: producto a barrel

--- models/index.js
+++ models/index.js
@@ -2,10 +2,12 @@ const Role = require('./role');
 const Categoria = require('./categoria');
 const Server = require('./server');
 const Usuario = require('./usuario');
+const Producto = require('./producto');
 
 module.exports = {
   Role,
   Categoria,
   Server,
   Usuario,
+  Producto,
 };
