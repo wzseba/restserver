73f65aad380f949a91b5b4c3a400d9e6e2ad9ef5
wzseba <wzseba@gmail.com>
2024-02-26 21:46:55 -0300

    add: validator para crear producto en desarrollo

--- validators/productoValidator.js
+++ validators/productoValidator.js
@@ -1,5 +1,5 @@
-const { param } = require('express-validator');
-const { existeProductoPorId } = require('./dbValidators');
+const { param, body } = require('express-validator');
+const { existeProductoPorId, existeCategoriaPorId } = require('./dbValidators');
 const { validationFieldResults } = require('./validatorField');
 
 const validatorIdProduct = [
@@ -8,6 +8,18 @@ const validatorIdProduct = [
   validationFieldResults,
 ];
 
+const validatorPostProducto = [
+  body('nombre', 'El nombre es obligatorio')
+    .trim()
+    .escape()
+    .isAlpha()
+    .isString(),
+  body('categoria', 'La categoria no es valida').isMongoId(),
+  body('categoria').custom(existeCategoriaPorId),
+  validationFieldResults,
+];
+
 module.exports = {
   validatorIdProduct,
+  validatorPostProducto,
 };
