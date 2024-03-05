36e7f2bf375f401dc936759599a41cdc43d8d27f
wzseba <wzseba@gmail.com>
2024-03-04 22:40:12 -0300

    add: funcion extensionPermitida

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -1,11 +1,18 @@
 const { response } = require('express');
+const { extensionPermitida } = require('../helpers/extensionVerify');
 const path = require('path');
 
-const cargarArchivo = (req, res = response) => {
+const cargarArchivo = async (req, res = response) => {
   if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
     return res.status(400).json({ msg: 'No hay archivo para cargar' });
   }
   const { archivo } = req.files;
+
+  try {
+    extensionPermitida(archivo);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
 
   const uploadPath = path.join(__dirname, '../uploads/', archivo.name);
 
