1a6fcab48d039179c0d56e4bdc7120d27cf454b0
wzseba <wzseba@gmail.com>
2024-03-05 23:01:18 -0300

    refactor: se separo la logica para cargar archivos y manejo de errores

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -1,26 +1,21 @@
 const { response } = require('express');
-const { extensionPermitida } = require('../helpers/extensionVerify');
-const path = require('path');
+const { subirArchivo } = require('../helpers');
 
 const cargarArchivo = async (req, res = response) => {
-  if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
-    return res.status(400).json({ msg: 'No hay archivo para cargar' });
-  }
-  const { archivo } = req.files;
-
   try {
-    extensionPermitida(archivo);
+    if (
+      !req.files ||
+      Object.keys(req.files).length === 0 ||
+      !req.files.archivo
+    ) {
+      return res.status(400).json({ msg: 'No hay archivo para cargar' });
+    }
+    const nombre = await subirArchivo(req.files);
+
+    res.json({ nombre });
   } catch (error) {
-    return res.status(400).json({ error: error.message });
+    res.status(400).json({ error: error.message });
   }
-
-  const uploadPath = path.join(__dirname, '../uploads/', archivo.name);
-
-  archivo.mv(uploadPath, function (err) {
-    if (err) return res.status(500).json(err);
-
-    res.json({ msg: 'Archivo cargado correctamente' });
-  });
 };
 
 module.exports = {
