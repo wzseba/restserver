fb898310573001e51b2811ed9e3dcfd9c6193409
wzseba <wzseba@gmail.com>
2024-03-06 22:28:38 -0300

    add: route actualizarImagen

--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -1,8 +1,13 @@
 const { Router } = require('express');
-const { cargarArchivo } = require('../controllers/upload.controller');
+const {
+  cargarArchivo,
+  actualizarImagen,
+} = require('../controllers/upload.controller');
+const { putUserImage } = require('../validators/uploadValidators');
 
 const router = Router();
 
 router.post('/', cargarArchivo);
+router.put('/:coleccion/:id', putUserImage, actualizarImagen);
 
 module.exports = router;
