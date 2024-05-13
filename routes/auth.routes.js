6d2566deb1f40b7e1b80abf2f8ddbe3fdbe68fce
wzseba <wzseba@gmail.com>
2024-05-12 23:01:56 -0300

    add: ruta renovarToken

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,15 +2,18 @@ const { Router } = require('express');
 const {
   loginUsuarios,
   googleSignIn,
+  renovarToken,
 } = require('../controllers/auth.controller');
 const {
   postAuthValidator,
   authGoogleSignIn,
 } = require('../validators/authValidators');
+const { validJTW } = require('../middlewares/jwtVerifications');
 
 const router = Router();
 
 router.post('/login', postAuthValidator, loginUsuarios);
 router.post('/google', authGoogleSignIn, googleSignIn);
+router.get('/', validJTW, renovarToken);
 
 module.exports = router;
