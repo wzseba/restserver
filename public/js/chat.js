19e9bea3f75afe62c1d3cf91c518503429becc42
wzseba <wzseba@gmail.com>
2024-05-13 23:08:27 -0300

    add: desarrollo de logica del chat con token

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,33 @@
 // const socket = io();
+
+let usuario = null;
+let socket = null;
+
+// Ver window.location.hostname.includes('localhost')
+const url = 'http://localhost:8080/api/auth/';
+
+// Validar token
+const validarToken = async () => {
+  const token = localStorage.getItem('token') || '';
+
+  if (token.length <= 10) {
+    window.location = 'index.html';
+    throw new Error('No hay token en el servidor');
+  }
+
+  const resp = await fetch(url, {
+    headers: { 'x-token': token },
+  });
+
+  const { usuario: usuarioDB, token: tokenDB } = await resp.json();
+  console.log(usuarioDB, tokenDB);
+  // Renovar token
+  localStorage.setItem('token', tokenDB);
+  usuario = usuarioDB;
+};
+
+const main = async () => {
+  await validarToken();
+};
+
+main();
