5b981954b42d65a42c0cd8d398e8eae4c23252d9
wzseba <wzseba@gmail.com>
2024-05-15 22:32:20 -0300

    add: funcion comprobar token

--- helpers/jsonWebToken.js
+++ helpers/jsonWebToken.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const { Usuario } = require('../models');
 
 const generarJWT = uid => {
   try {
@@ -12,6 +13,27 @@ const generarJWT = uid => {
   }
 };
 
+const comprobarJWT = async (token = '') => {
+  try {
+    if (token.length < 10) return null;
+
+    const { uid } = jwt.verify(token, process.env.SECRET_KEY);
+
+    const usuario = await Usuario.findById(uid);
+
+    if (usuario) {
+      if (usuario.estado) {
+        return usuario;
+      } else {
+        return null;
+      }
+    }
+  } catch (error) {
+    return null;
+  }
+};
+
 module.exports = {
   generarJWT,
+  comprobarJWT,
 };
