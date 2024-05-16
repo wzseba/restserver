a18d2cf9529b2dc547363e26116480dd407ca9d4
wzseba <wzseba@gmail.com>
2024-05-15 22:33:49 -0300

    add: logica para conectar y desconectar usuario

---
+++ sockets/controller.js
@@ -0,0 +1,14 @@
+const { Socket } = require('socket.io');
+const { comprobarJWT } = require('../helpers');
+
+const socketController = async (socket = new Socket()) => {
+  const usuario = await comprobarJWT(socket.handshake.headers['x-token']);
+
+  if (!usuario) {
+    return socket.disconnect();
+  }
+
+  console.log('Se conecto', usuario.nombre);
+};
+
+module.exports = { socketController };
