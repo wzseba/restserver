41bb7ca65511e1963a3585e12aa521638cd2c6e2
wzseba <wzseba@gmail.com>
2024-05-27 21:22:01 -0300

    add: agregar y limpiar usuario

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,14 +1,25 @@
 const { Socket } = require('socket.io');
 const { comprobarJWT } = require('../helpers');
+const { ChatMensaje } = require('../models');
 
-const socketController = async (socket = new Socket()) => {
+const chatMensajes = new ChatMensaje();
+
+const socketController = async (socket = new Socket(), io) => {
   const usuario = await comprobarJWT(socket.handshake.headers['x-token']);
 
   if (!usuario) {
     return socket.disconnect();
   }
 
-  console.log('Se conecto', usuario.nombre);
+  // Agregar el usuario conectado
+  chatMensajes.conectarUsuario(usuario);
+  io.emit('usuarios-activos', chatMensajes.usuariosArray);
+
+  // Limpiar usuario cuando se desconecta
+  socket.on('disconnect', () => {
+    chatMensajes.desconectarUsuario(usuario.id);
+    io.emit('usuarios-activos', chatMensajes.usuariosArray);
+  });
 };
 
 module.exports = { socketController };
